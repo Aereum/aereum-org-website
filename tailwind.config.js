c13cb2dad445e73a04ad5962f8438443ddd7bd74
lienko <larigdtk@gmail.com>
2022-02-06 10:31:34 -0300

    home design start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'tc0': '#ffc2c7', // textcolors
+      'tc1': '#b6e5d8',
+      'tc2': '#fbe5c8',
+      'tc3': '#8fdde7',
+      'tc4': '#d3bbdd',
+      'tc5': '#efe7d3',
+      'tc6': '#bbe7fe',
+      'bc0': '#1572A1',
+    },
+    fill: ({theme}) => ({
+      bg: theme('colors.bc0')
+
+    }),
+    fontFamily: {
+      'sans': ['ui-sans-serif'],
+      'mono':['SFMono-Regular'],
+      'serif':['Georgia'],
+    },
     extend: {},
   },
   plugins: [],
